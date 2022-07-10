2d81129821e1626246e891c52c4dcf82bc08edbe
Gagan Sharma <gagansharma2717@gmail.com>
2022-07-10 22:56:08 +0530

    Added Generic Tree Questions

--- src/Generic_Tree/LevelOrderLineWise.java
+++ src/Generic_Tree/LevelOrderLineWise.java
@@ -43,25 +43,47 @@ public class LevelOrderLineWise {
         // when main queue will become empty then we will make child queue as main queue
         // and will re-initialize child queue
         mq.add(root);
-        while(mq.size() > 0 ){
+        while (mq.size() > 0) {
             root = mq.remove();
             System.out.print(root.data + " ");
 
-            for(Node child : root.children){
+            for (Node child : root.children) {
                 cq.add(child);
             }
 
-            if(mq.size() == 0){
+            if (mq.size() == 0) {
                 mq = cq;
                 cq = new ArrayDeque<>();
                 System.out.println();
             }
         }
+    }
+    public static void levelOrderLineWise2(Node root) {
+        Queue<Node> mq = new ArrayDeque<>();
+        mq.add(root);
 
+        while (mq.size() > 0) {
+            // chidlren in current level
+            int cicl = mq.size();
+
+            // loop from 0 to children in current level
+            for (int i = 0; i < cicl; i++) {
+                // remove print add children
+                root = mq.remove();
+                System.out.print(root.data + " ");
+
+                for (Node child : root.children) {
+                    mq.add(child);
+                }
+            }
+            System.out.println();
+        }
     }
     public static void main(String[] args) {
         int[] arr = {10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
         Node root = construct(arr);
         levelOrderLineWise(root);
+        System.out.println("Approach 2");
+        levelOrderLineWise2(root);
     }
 }
